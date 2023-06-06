fc68b68e4f109c7efbf5453543b540616dbb0692
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-06 16:41:51 +0000

    4️⃣❌4️⃣

---
+++ w1/src/app/(lessons)/v22/page.tsx
@@ -0,0 +1,61 @@
+/**
+ * This page is going to be all the work for the tailwind sandbox
+ */
+
+import Showi from "../components/shb"
+import Image from "next/image"
+
+export default function Tws() {
+  return (
+    <main>
+      <Showi />
+      <h1 className="  flex justify-center bg-fuchsia-400 pb-2 pt-2 text-lg font-extrabold p-5 ">
+        EX21 - Tranform - This one in
+      </h1>
+      <p className="m-1 flex justify-center divide-x-8 divide-zinc-200 bg-slate-200 italic">
+        Transforms and Transitions
+      </p>
+      {/**============================Actual code below======================================= */}
+      {/**============================Actual code below======================================= */}
+      {/**============================Actual code below======================================= */}
+      {/**============================Actual code below=====bvc ================================== */}
+    
+      <h1 className="  flex justify-center bg-slate-500- pb-2 pt-2 text-lg font-extrabold p-5 ">
+        SVG Animation 
+      </h1>
+
+      <body>
+    <div class="flex items-center justify-center bg-slate-900">
+      <svg
+        class="w-48 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    </div>
+  </body>
+
+
+
+
+      {/**============================Actual code below======================================= */}
+      {/**============================Actual code below======================================= */}
+      {/**============================Actual code below======================================= */}
+      {/**============================Actual code below=====bvc ================================== */}
+    </main>
+  )
+}
