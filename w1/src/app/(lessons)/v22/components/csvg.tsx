f7068c57760795bd6b6193d692299e31463a909f
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-06-06 17:11:46 +0000

    4️⃣❌4️⃣

---
+++ w1/src/app/(lessons)/v22/components/csvg.tsx
@@ -0,0 +1,58 @@
+export default function Circ() {
+  return (
+    <section>
+      <h1 className="text-2xl text-center bg-green-500">
+        Circle 1{" "}
+        <code className="bg-yellow-300 text-black tracking-widest">
+          Spinning
+        </code>{" "}
+      </h1>
+      <div className="flex flex-col p-3 items-center justify-center  bg-slate-900">
+        <svg
+          className="w-48 animate-spin text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </div>
+
+      <h1 className="text-2xl text-center bg-red-500">Circle 2</h1>
+      <div className="flex flex-col p-3 items-center justify-center  bg-slate-900">
+        <svg
+          className="w-48 animate-pulse text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </div>
+    </section>
+  );
+}
--- w1/src/app/(lessons)/v22/page.tsx
+++ w1/src/app/(lessons)/v22/page.tsx
@@ -4,6 +4,9 @@
 
 import Showi from "../components/shb"
 import Image from "next/image"
+import Circ from "./components/csvg"
+
+
 
 export default function Tws() {
   return (
@@ -23,31 +26,14 @@ export default function Tws() {
       <h1 className="  flex justify-center bg-slate-500- pb-2 pt-2 text-lg font-extrabold p-5 ">
         SVG Animation 
       </h1>
+      
+      <Circ />
 
-      <body>
-    <div className="flex items-center justify-center bg-slate-900">
-      <svg
-        className="w-48 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          className="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    </div>
-  </body>
+
+
+
+   
+  
 
 
 
